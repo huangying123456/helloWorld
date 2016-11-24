3b3ae18352a3a2ed1e5ed91a034ab1cbf54d59be
edz <edz@edzdeMacBook-Air.local>
2016-11-24 17:32:50 +0800

    Initial commit

---
+++ src/User.java
@@ -0,0 +1,29 @@
+import com.company.Main;
+
+/**
+ * Created by edz on 2016/11/23.
+ */
+
+public class User {
+
+    private  int  age;
+    private  String name;
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+
+    }
+
+}
