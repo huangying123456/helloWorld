5cf04d7513949fd981b38fc779f543fa5ad35c09
huangying123456 <15617679341@163.com>
2016-11-30 11:50:52 +0800

    test of merge

--- src/com/company/aa/User3.java
+++ src/com/company/aa/User3.java
@@ -4,4 +4,6 @@ package com.company.aa;
  * Created by hy on 2016/11/29.
  */
 public class User3 {
-}
+    public static void main(String[] args){
+        System.out.print("测试download,pull,push,merge");
+    }}
